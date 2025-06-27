49e4dd1d5c60767eba542e92cbb3e6dc7d8cf353
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-06-26 23:33:56 -0500

    refactor: remove unused imports from PromptGalleryTab component

--- src/components/PromptGalleryTab.tsx
+++ src/components/PromptGalleryTab.tsx
@@ -8,7 +8,6 @@ import { Input } from "@/components/ui/input";
 import { 
   Search, 
   Copy, 
-  Star, 
   FileText, 
   Mail, 
   Code, 
@@ -20,7 +19,6 @@ import {
   X
 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { PROMPT_GALLERY_TEMPLATES } from "@/constants";
 
 interface Template {
   id: string;
