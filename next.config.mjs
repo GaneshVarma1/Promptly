4ad9beacf4f8159e2d8c4c5f9238d32e34352259
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-07-16 01:01:41 -0500

    Apply all pending changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_HUGGING_FACE_API_KEY:
-      process.env.NEXT_PUBLIC_HUGGING_FACE_API_KEY,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
